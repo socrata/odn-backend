b9186bb8173ed2c3db1bae7b4374296bed722bbb
Lane Aasen <laneaasen@gmail.com>
2016-06-22 16:26:21 -0700

    start of API tests

---
+++ test/home.js
@@ -0,0 +1,14 @@
+
+const chakram = require('chakram');
+
+function url(path) {
+    return `http://localhost:3001${path}`;
+}
+
+describe('Home page', () => {
+    it('Should give a 200', () => {
+        const response = chakram.get(url('/'));
+        return chakram.expect(response).to.have.status(200);
+    });
+});
+
