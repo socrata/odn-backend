845875b68a367567e2447359b4e9d21a9d740471
Lane Aasen <laneaasen@gmail.com>
2016-06-22 18:01:41 -0700

    finished tests for related

--- test/related.js
+++ test/related.js
@@ -71,10 +71,202 @@ describe('/related', () => {
         return expect(related('parent?id=0400000US53&limit=asd')).to.have.status(422);
     });
 
-    it('should have the correct schema', () => {
-        const response = related('parent?id=0400000US53');
-        expect(response).to.have.schema(relatedSchema);
-        expect(response).to.have.status(200);
-        return chakram.wait();
+    it('should show that the pacific division is a parent of washington', () => {
+        return related('parent?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.have.json({
+                'entity': {
+                    'id': '0400000US53',
+                    'name': 'Washington',
+                    'type': 'state'
+                },
+
+                'groups': [
+                    {
+                        'type': 'division',
+                        'entities': [
+                            {
+                                'id': '0300000US9',
+                                'name': 'Pacific Division',
+                                'type': 'division'
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+    });
+
+    it('should show that king county, seattle city, and seattle metro are children of washington', () => {
+        return related('child?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json({
+                'entity': {
+                    'name': 'Washington',
+                    'type': 'state'
+                },
+
+                'groups': [
+                    {
+                        'type': 'county',
+                        'entities': [
+                            {
+                                'name': 'King County, WA',
+                                'type': 'county'
+                            }
+                        ]
+                    },
+                    {
+                        'type': 'place',
+                        'entities': [
+                            {
+                                'name': 'Seattle, WA',
+                                'type': 'place'
+                            }
+                        ]
+                    },
+                    {
+                        'type': 'msa',
+                        'entities': [
+                            {
+                                'name': 'Seattle Metro Area (WA)',
+                                'type': 'msa'
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+    });
+
+    it('should respect the length parameter', () => {
+        return related('child?id=0400000US53&limit=33').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            response.body.groups.forEach(group => {
+                expect(group.entities).to.have.length.below(34);
+            });
+        });
+    });
+
+    it('should show that california is a sibling of washington', () => {
+        return related('sibling?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json({
+                'entity': {
+                    'name': 'Washington',
+                    'type': 'state'
+                },
+
+                'groups': [
+                    {
+                        'type': 'state',
+                        'entities': [
+                            {
+                                'name': 'California',
+                                'type': 'state'
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+    });
+
+    it('should show that oregon is a peer of washington', () => {
+        return related('peer?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json({
+                'entity': {
+                    'name': 'Washington',
+                    'type': 'state'
+                },
+
+                'groups': [
+                    {
+                        'type': 'state',
+                        'entities': [
+                            {
+                                'name': 'Colorado',
+                                'type': 'state'
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+    });
+
+    it('should show that the united states has no parents', () => {
+        return related('parent?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json('groups', []);
+        });
+    });
+
+    it('should show that the united states has no peers', () => {
+        return related('parent?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json('groups', []);
+        });
+    });
+
+    it('should show that the united states has no siblings', () => {
+        return related('parent?id=0400000US53').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json('groups', []);
+        });
+    });
+
+    it('should show that seattle has no children', () => {
+        return related('child?id=1600000US5363000').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json('groups', []);
+        });
+    });
+
+    it('should show that the new york metro area has many parents', () => {
+        return related('parent?id=310M200US35620').then(response => {
+            expect(response).to.have.status(200);
+            expect(response).to.have.schema(relatedSchema);
+            expect(response).to.comprise.of.json({
+                'entity': {
+                    'id': '310M200US35620',
+                    'name': 'New York-Newark-Jersey City, NY-NJ-PA Metro Area',
+                    'type': 'msa'
+                },
+                'groups': [
+                    {
+                        'type': 'state',
+                        'entities': [
+                            {
+                                'id': '0400000US36',
+                                'name': 'New York',
+                                'type': 'state'
+                            },
+                            {
+                                'id': '0400000US42',
+                                'name': 'Pennsylvania',
+                                'type': 'state'
+                            },
+                            {
+                                'id': '0400000US34',
+                                'name': 'New Jersey',
+                                'type': 'state'
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
     });
 });
+
