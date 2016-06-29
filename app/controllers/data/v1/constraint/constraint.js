57ac500f8b6d397dca63ea23ffb8b0b885ef618b
Lane Aasen <laneaasen@gmail.com>
2016-06-29 13:58:09 -0700

    refactor constraint

---
+++ app/controllers/data/v1/constraint/constraint.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const _ = require('lodash');
+
+const Availability = require('../availability/availability');
+const Exception = require('../../../error');
+
+function getDataset(tree, path) {
+    if (_.isNil(path) || _.isNil(tree) || path.length === 0) return null;
+    if (path.length === 1) return null;
+
+    const id = path[0];
+    const subtree = tree[id];
+    if (path.length === 2 && 'variables' in subtree) return subtree;
+
+    const subpath = _.tail(path);
+    if ('topics' in subtree) return getDataset(subtree.topics, subpath);
+    if ('datasets' in subtree) return getDataset(subtree.datasets, subpath);
+
+    return null;
+}
+
+function getVariable(dataset, path) {
+    if (_.isNil(dataset)) return null;
+    const id = _.last(path);
+    return dataset.variables[id];
+}
+
+class Constraint {
+    static parseID(entities, variableID) {
+        const path = variableID.split('.');
+        const topicTree = Availability.topicTree([variableID], entities);
+        const dataset = getDataset(topicTree, path);
+        const variable = getVariable(dataset, path);
+
+        return [dataset, variable];
+    }
+
+    static validateConstraints(dataset, constraint, constraints) {
+        return new Promise((resolve, reject) => {
+            _.forIn(constraints, (value, name) => {
+                if (!_.includes(dataset.constraints, name))
+                    reject(Exception.notFound(`invalid constraint: ${name}.
+                        Must be one of ${dataset.constraints.join(', ')}`));
+                if (name === constraint)
+                    reject(Exception.invalidParam(`cannot specify a
+                        value for ${constraint}`));
+            });
+
+            if (_.keys(constraints).length !== dataset.constraints.length - 1)
+                reject(Exception.invalidParam(`ambiguous input. must specify values for
+                    ${_.without(dataset.constraints, constraint).join(', ')}`));
+
+            resolve();
+        });
+    }
+}
+
+module.exports = Constraint;
+
--- app/controllers/data/v1/constraint/controller.js
+++ app/controllers/data/v1/constraint/controller.js
@@ -5,38 +5,8 @@ const querystring = require('querystring');
 
 const EntityLookup = require('../../../../entity-lookup');
 const Exception = require('../../../error');
-const Availability = require('../availability/availability');
 const Request = require('../../../../request');
-
-function getVariable(tree, path) {
-    if (_.isNil(path) || _.isNil(tree) || path.length === 0) return null;
-    if (path.length === 1) return null;
-
-    const id = path[0];
-    const subtree = tree[id];
-    if (path.length === 2 && 'variables' in subtree) return subtree.variables[path[1]];
-
-    const subpath = _.tail(path);
-    if ('topics' in subtree) return getVariable(subtree.topics, subpath);
-    if ('datasets' in subtree) return getVariable(subtree.datasets, subpath);
-
-    return null;
-}
-
-function getDataset(tree, path) {
-    if (_.isNil(path) || _.isNil(tree) || path.length === 0) return null;
-    if (path.length === 1) return null;
-
-    const id = path[0];
-    const subtree = tree[id];
-    if (path.length === 2 && 'variables' in subtree) return subtree;
-
-    const subpath = _.tail(path);
-    if ('topics' in subtree) return getDataset(subtree.topics, subpath);
-    if ('datasets' in subtree) return getDataset(subtree.datasets, subpath);
-
-    return null;
-}
+const Constraint = require('./constraint');
 
 module.exports = (request, response) => {
     const errorHandler = Exception.getHandler(request, response);
@@ -53,57 +23,38 @@ module.exports = (request, response) => {
         if (entities.length === 0)
             return errorHandler(Exception.invalidParam('at least one id required'));
 
-        const topicTree = Availability.topicTree([variableID], entities);
-
-        const variable = getVariable(topicTree, variableID.split('.'));
-        if (_.isNil(variable))
+        const [dataset, variable] = Constraint.parseID(entities, variableID);
+        if (_.some([dataset, variable], _.isNil))
             return errorHandler(Exception.notFound(`invalid variable id: ${variableID}`));
-
-        const dataset = getDataset(topicTree, variableID.split('.'));
         if (!_.includes(dataset.constraints, constraint))
-            return errorHandler(Exception.notFound(`invalid constraint: ${constraint}. Must be one of ${dataset.constraints.join(', ')}`));
+            return errorHandler(Exception.notFound(`invalid constraint: ${constraint}.
+                        Must be one of ${dataset.constraints.join(', ')}`));
 
         const constraints = _.omit(request.query, ['id', 'constraint']);
-        let error = false;
-        _.forIn(constraints, (value, name) => {
-            if (error) return;
 
-            if (!_.includes(dataset.constraints, name)) {
-                error = true;
-                return errorHandler(Exception.notFound(`invalid constraint: ${name}. Must be one of ${dataset.constraints.join(', ')}`));
-            }
+        Constraint.validateConstraints(dataset, constraint, constraints).then(() => {
+            const params = _.assign({
+                '$group': constraint,
+                '$select': constraint,
+                '$order': `${constraint} ASC`
+            }, constraints);
 
-            if (name === constraint) {
-                error = true;
-                return errorHandler(Exception.invalidParam(`cannot specify a value for ${constraint}`));
-            }
-        });
-        if (error) return;
+            const url = `${variable.url}&${querystring.stringify(params)}`;
+            Request.getJSON(url).then(json => {
+                const options = json.map(option => {
+                    const value = option[constraint];
+                    const params = _.assign({
+                        [constraint]: value,
+                    }, constraints);
 
-        if (_.keys(constraints).length !== dataset.constraints.length - 1)
-            return errorHandler(Exception.invalidParam(`ambiguous input. must specify values for ${_.without(dataset.constraints, constraint).join(', ')}`));
+                    return {
+                        constraintValue: value,
+                        constraintURL: `${variable.url}&${querystring.stringify(params)}`
+                    };
+                });
 
-        const params = _.assign({
-            '$group': constraint,
-            '$select': constraint,
-            '$order': `${constraint} ASC`
-        }, constraints);
-
-        const url = `${variable.url}&${querystring.stringify(params)}`;
-        Request.getJSON(url).then(json => {
-            const options = json.map(option => {
-                const value = option[constraint];
-                const params = _.assign({
-                    [constraint]: value,
-                }, constraints);
-
-                return {
-                    constraintValue: value,
-                    constraintURL: `${variable.url}&${querystring.stringify(params)}`
-                };
-            });
-
-            response.json({permutations: options});
+                response.json({permutations: options});
+            }).catch(errorHandler);
         }).catch(errorHandler);
     }).catch(errorHandler);
 };
