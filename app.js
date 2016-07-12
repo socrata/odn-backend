df28f3dfae20abb0c7398ec0bc6ac396447ef3b8
Lane Aasen <laneaasen@gmail.com>
2016-07-12 12:00:15 -0700

    rename map controller to map values

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/data/v1/availability', require('./app/data/availability/controller'));
 app.get('/data/v1/constraint/:variable', require('./app/data/constraint/controller'));
 app.get('/data/v1/values', require('./app/data/values/controller'));
 app.get('/data/v1/map/new', require('./app/data/map/new'));
-app.get('/data/v1/map/values', require('./app/data/map/controller'));
+app.get('/data/v1/map/values', require('./app/data/map/values'));
 app.get('/suggest/v1/:type', require('./app/suggest/controller'));
 
 app.use(require('./app/error').respond);
