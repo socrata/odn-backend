0305795de1b096b1b2cb31411c1b60d45fd02dba
Lane Aasen <laneaasen@gmail.com>
2016-07-13 09:25:04 -0700

    moves stopwords to json

--- app/stopwords.js
+++ app/stopwords.js
@@ -1,66 +1,34 @@
 'use strict';
 
-const stopwords = new Set([
-    'and',
-    'is',
-    'or',
-    'the',
-    'in',
-    'of',
-    'that',
-    'percent',
-    'personal',
-    'count',
-    'rate',
-    'mean',
-    'median',
-    'average',
-    'with',
-    'to',
-    'over',
-    'under',
-    'higher',
-    'lower',
-    'per',
-    'related',
-    'total',
-    'ratio',
-    'capita',
-    'annual',
-    'overall',
-    'other',
-    'index',
-    'what',
-    'what\'s',
-    'whats',
-    'wat',
-    'where',
-    'when',
-    'for',
-    'at',
-    'are'
-]);
+const fs = require('fs');
 
 class Stopwords {
     /**
+     * List of stopwords
+     */
+    constructor(stopwords) {
+        this.stopwords = new Set(stopwords);
+    }
+    /**
      * Extracts all important words from a string ignoring all stopwords.
      */
-    static importantWords(string) {
+    importantWords(string) {
         return string
             .replace(/[\.,]/g, '')
             .replace(/[\\\/]/g, ' ')
             .toLowerCase()
             .split(' ')
-            .filter(word => !stopwords.has(word));
+            .filter(word => !this.stopwords.has(word));
     }
 
     /**
      * Strips all stopwords from the string.
      */
-    static strip(string) {
-        return Stopwords.importantWords(string).join(' ');
+    strip(string) {
+        return this.importantWords(string).join(' ');
     }
 }
 
-module.exports = Stopwords;
+const stopwordsJSON = fs.readFileSync('data/stopwords.json');
+module.exports = new Stopwords(JSON.parse(stopwordsJSON));
 
