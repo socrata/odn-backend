b49b720f95ae39bf2ecf94a52eef41b4ac81e174
Lane Aasen <laneaasen@gmail.com>
2016-07-19 17:31:46 -0700

    load test works for map values now

--- load-test.sh
+++ load-test.sh
@@ -2,5 +2,5 @@
 
 ./flush-memcache.sh
 node test/load/generate-urls.js 1000 > .load-test-urls
-siege -f .load-test-urls -d0 -r10 -c25
+siege -f .load-test-urls -d1 -r2 -c100
 
--- test/load/generate-urls.js
+++ test/load/generate-urls.js
@@ -1,5 +1,9 @@
 
 const _ = require('lodash');
+const request = require('request-promise');
+
+const SOQL = require('../../app/soql');
+const Constants = require('../../app/constants');
 
 /**
  * Generate random URLs for load testing.
@@ -9,19 +13,27 @@ const baseURL = 'http://localhost:3001';
 
 const options = {
     app_token: ['TULfSVvj7mto3wKM3qW8dMj9L'],
-    entity_id: ['0100000US', '0400000US53'],
     relation_type: ['parent', 'child', 'sibling', 'peer'],
     suggest_type: ['entity', 'category', 'publisher', 'question', 'dataset'],
     search_type: ['question', 'dataset'],
-    query: ['seattle'],
+    query: ['seattle', 'crime', 'washington', '', 'a', 'b', 'publisher', 'category', 'health', 'king', 'new york', 'fun', 'cool data', 'wow'],
     dataset_id: ['demographics.population'],
     variable: ['demographics.population.change', 'demographics.population.count'],
     constraint: ['year'],
     forecast: _.range(0, 10),
     describe: ['true', 'false'],
-    year: _.range(2010, 2014)
-};
+    year: _.range(2010, 2014),
+    zoom_level: _.range(1, 18),
+    bounds: () => {
+        const nwLat = _.random(24.396308, 49.384358, true);
+        const seLat = _.random(24.396308, nwLat, true);
 
+        const nwLong = _.random(-124.848974, -66.885444, true);
+        const seLong = _.random(nwLong, -66.885444, true);
+
+        return [nwLat, nwLong, seLat, seLong].join(',');
+    }
+};
 
 const paths = [
     ['suggest/v1/{suggest_type}', ['query']],
@@ -31,11 +43,45 @@ const paths = [
     ['data/v1/availability', ['entity_id']],
     ['data/v1/constraint/{variable}', ['entity_id', 'constraint']],
     ['data/v1/values', ['variable', 'entity_id', 'forecast']],
-    ['data/v1/map/new', ['variable', 'entity_id', 'year']]
+    ['data/v1/map/new', ['variable', 'entity_id', 'year']],
+    ['data/v1/map/values', ['session_id', 'zoom_level', 'bounds']]
 ];
 
+function setup() {
+    return generateEntities()
+        .then(entities => {
+            options.entity_id = entities;
+            return Promise.resolve();
+        })
+        .then(() => generateMapSessions(10))
+        .then(sessions => {
+            options.session_id = sessions;
+            return Promise.resolve();
+        });
+}
+
+function generateEntities() {
+    return new SOQL(Constants.ENTITY_URL)
+        .token(options.app_token[0])
+        .select('id')
+        .send()
+        .then(entities => {
+            return Promise.resolve(entities.map(_.property('id')));
+        });
+}
+
+function generateMapSessions(n) {
+    const path = ['data/v1/map/new', ['variable', 'entity_id', 'year']];
+    return Promise.all(_.times(n, () => request(generateURL(path)))).then(responses => {
+        return Promise.resolve(responses.map(JSON.parse).map(_.property('session_id')));
+    });
+}
+
 function generateOption(type) {
-    return _.sample(options[type]);
+    const a = options[type];
+
+    if (_.isArray(a)) return _.sample(options[type]);
+    if (_.isFunction(a)) return a.apply(this);
 }
 
 function generateParams(types) {
@@ -56,12 +102,14 @@ function generatePath(path) {
     });
 }
 
-function generateURL() {
-    const [path, params] = _.sample(paths);
-
+function generateURL(tuple) {
+    const [path, params] = tuple;
     return `${baseURL}/${generatePath(path)}?${generateParams(params)}`;
 }
 
+function randomURL() {
+    return generateURL(_.sample(paths));
+}
 
 const args = process.argv.slice(2);
 
@@ -71,6 +119,9 @@ if (args.length !== 1) {
 } else {
     const [n] = args;
 
-    _.times(n, generateURL).forEach(url => console.log(url));
+    setup().then(() => {
+        _.times(n, randomURL).forEach(url => console.log(url));
+        process.exit(0);
+    }).catch(error => console.error(error));
 }
 
