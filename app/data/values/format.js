8bfe8ae50fc88a47ac1e950ee1391f0284148e15
Lane Aasen <laneaasen@gmail.com>
2016-09-13 11:19:19 -0700

    adds precision{n} formatter and fixes crime incident rate formatting

--- app/data/values/format.js
+++ app/data/values/format.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash');
 const numeral = require('numeral');
 
 const numeralFormatters = {
@@ -11,10 +12,31 @@ const numeralFormatters = {
     per1000: '0,0.00%'
 };
 
+const precisionPrefix = 'precision';
+
 function format(type) {
     if (type === 'percent') return n => numeral(n / 100).format(numeralFormatters.percent);
     if (type === 'per1000') return n => numeral(n / 100000).format(numeralFormatters.percent);
-    return number => numeral(number).format(numeralFormatters[type] || '');
+    const formatter = numeralFormatters[type] || precisionFormatter(type) || '';
+    return number => numeral(number).format(formatter);
+}
+
+
+function precisionFormatter(type) {
+    if (isPrecision(type))
+        return `0,0.${_.repeat('0', getPrecision(type))}`;
+    return null;
+}
+
+// 'precision3' => true
+// 'percent' => false
+function isPrecision(type) {
+    return _.startsWith(type, precisionPrefix);
+}
+
+// 'precision3' => 3
+function getPrecision(type) {
+    return parseInt(type.substring(precisionPrefix.length));
 }
 
 module.exports = format;
