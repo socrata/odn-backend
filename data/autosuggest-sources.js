b82e1bdf8800e9f6597c7774ae09856e0a5ca6d3
Lane Aasen <laneaasen@gmail.com>
2016-09-23 12:32:34 -0700

    removes entity and question autosuggest sources

--- data/autosuggest-sources.js
+++ data/autosuggest-sources.js
@@ -6,33 +6,6 @@ const AutosuggestDataset = require('../app/suggest/autosuggest-dataset');
 const Sources = require('../app/sources');
 
 const declarations = {
-    // This is only for /search/v1/questions, which is now only used
-    // to generate random questions on the ODN home page.
-    question: {
-        domain: Constants.ODN_DATA_DOMAIN,
-        fxf: 'arys-69tf',
-        column: 'question',
-        encoded: ['regionName', 'regionID', 'regionPopulation',
-                  'vector', 'source', 'variable', 'metric', 'index'],
-        sort: option => {
-            const population = parseFloat(option.fields.regionPopulation);
-            const index = parseFloat(option.fields.index);
-            return -(population - index);
-        },
-        transform: option => {
-            return {
-                entity: {
-                    id: option.fields.regionID,
-                    name: option.fields.regionName
-                },
-                vector: option.fields.vector,
-                metric: option.fields.metric,
-                variable_name: option.fields.variable,
-                text: `What is the ${option.fields.variable} of ${option.fields.regionName}?`
-            };
-        }
-    },
-
     dataset: {
         domain: Constants.ODN_DATA_DOMAIN,
         fxf: 'fpum-bjbr',
@@ -68,34 +41,6 @@ const declarations = {
             };
         }
     }
-};
-
-const occupationNames = {
-    'media': 'Media',
-    'engineering': 'Engineering',
-    'office_and_administration': 'Office and Administration',
-    'health_support': 'Health Support',
-    'sales': 'Sales',
-    'social_sciences': 'Social Sciences',
-    'healthcare': 'Healthcare',
-    'computers_and_math': 'Computers and Math',
-    'repair': 'Repair',
-    'material_moving': 'Material Moving',
-    'construction_and_extraction': 'Construction and Extraction',
-    'fire_fighting': 'Fire Fighting',
-    'social_services': 'Social Services',
-    'production': 'Production',
-    'management': 'Management',
-    'personal_care': 'Personal Care',
-    'education': 'Education',
-    'health_technicians': 'Health Technicians',
-    'transportation': 'Transportation',
-    'law_enforcement': 'Law Enforcement',
-    'janitorial': 'Janitorial',
-    'food_service': 'Food Service',
-    'farming_fishing_forestry': 'Farming, Fishing, Foresty',
-    'legal': 'Legal',
-    'business_and_finance': 'Business and Finance'
 };
 
 const datasets = _.mapValues(declarations, (declaration, id) => {
